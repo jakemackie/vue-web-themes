0a28d5a7821025bc79c6d3b940621f1451250819
jakemackie <jake.wmackie@gmail.com>
2024-03-06 02:25:37 +0000

    Updated readme and showcase

--- src/components/themeContentArea.vue
+++ src/components/themeContentArea.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="overflow-y-scroll fixed top-0 h-1/2 w-full">
+    <div class="fixed top-0 h-1/2 w-full">
         <div class="grid place-items-center py-10">
             <slot></slot>
         </div>
